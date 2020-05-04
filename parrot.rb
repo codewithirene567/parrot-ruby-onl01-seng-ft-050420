c50902c3d573323859563eb61a97ae91fc9b2284
Irene Scott <irenejinscott@gmail.com>
2020-05-04 23:58:40 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,12 +1,6 @@
-# Create method `parrot` that outputs a given phrase and
-# returns the phrase
 def parrot(phrase, phrase="Squawk!")
   puts "phrase"
-  return "phrase"
+  return phrase
 end
   
-  def print_and_return_name
-  puts "Guy Fieri"
-  "Guy Fieri"
-end
-
+ 
