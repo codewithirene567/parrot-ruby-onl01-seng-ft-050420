85ccb91bf97990092be2d28b5533a8cc4d594e8c
Irene Scott <irenejinscott@gmail.com>
2020-05-05 00:16:48 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,11 @@
 def parrot(string, phrase="Squawk!")
   string = "Polly want cracker with #{phrase}"
-  puts "Polly want cracker with #{phrase}"
+  puts "string #{phrase}"
   return phrase
 end
-  
+  def stylish_chef
+  best_hairstyle = "Guy Fieri"
+  return "Martha Stewart"
+  "Guy Fieri"
+end
  
